011295dae02ba9ee093d0691b8366b17383dcc0e
nipscern <nipscernlab@gmail.com>
2024-08-16 10:47:43 -0300

    Download Page v2.0

--- javascript/js_download.js
+++ javascript/js_download.js
@@ -1,9 +1,52 @@
-document.getElementById("downloadBtn").addEventListener("click", function() {
-    const fileUrl = "https://github.com/nipscernlab/sapho/raw/main/Sapho_atual/Setup/inno/SAPHO.exe";
-    const a = document.createElement("a");
-    a.href = fileUrl;
-    a.download = "SAPHO.exe";  // Name of the file after download
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+document.addEventListener("DOMContentLoaded", function () {
+    let translations;
+    let preferredLanguage = localStorage.getItem("preferredLanguage") || "en";
+
+    function applyTranslations(language, translations) {
+        const elements = document.querySelectorAll("[data-i18n]");
+        elements.forEach(function (element) {
+            const key = element.getAttribute("data-i18n");
+            if (translations[language] && translations[language][key]) {
+                if (element.tagName.toLowerCase() === "title") {
+                    document.title = translations[language][key];
+                } else if (element.id === "downloadBtn") {
+                    element.textContent = translations[language][key];
+                } else {
+                    element.textContent = translations[language][key];
+                }
+            }
+        });
+        localStorage.setItem("preferredLanguage", language);
+    }
+
+    fetch("/json/json_idioma.json")
+        .then(response => response.json())
+        .then(data => {
+            translations = data;
+            if (!Object.keys(translations).includes(preferredLanguage)) {
+                preferredLanguage = "en";
+            }
+            applyTranslations(preferredLanguage, translations);
+
+            const languageButtons = document.querySelectorAll(".language-selector .language-option");
+            languageButtons.forEach(button => {
+                button.addEventListener("click", function () {
+                    const language = this.getAttribute("data-lang");
+                    applyTranslations(language, translations);
+                });
+            });
+        })
+        .catch(error => {
+            console.error("Error loading translations:", error);
+        });
+
+    document.getElementById("downloadBtn").addEventListener("click", function () {
+        const fileUrl = "https://github.com/nipscernlab/sapho/raw/main/Sapho_atual/Setup/inno/SAPHO.exe";
+        const a = document.createElement("a");
+        a.href = fileUrl;
+        a.download = "SAPHO.exe";  // Name of the file after download
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
 });
